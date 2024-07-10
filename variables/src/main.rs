4971000c6f76a8291bdbc2c6ba46f15bf9a7c6bd
adedotxn <adedotxn.dev@gmail.com>
2024-07-10 01:13:50 +0100

    Chapter 8: Common Collections

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
@@ -11,7 +13,6 @@ fn main() {
     let y = y + 1; // shadowed y
     println!("The value of shadowed y is: {y}");
 
-
     {
         let y = y * 2;
         println!("The value of y in the inner scope is: {y}");
@@ -19,8 +20,7 @@ fn main() {
 
     println!("The value of y in the global scope is: {y}");
 
-
-    let tup : (i32, f64, u8) = (500, 6.4, 1);
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
     let (a, b, c) = tup; // pattern matching to destructure the tuple
     println!("The value of a is: {a}");
 
@@ -30,7 +30,63 @@ fn main() {
     let array: [i32; 5] = [1, 2, 3, 4, 5];
     let array_of_same_values = [3; 5]; // [3, 3, 3, 3, 3]
 
-}
+    // Vectors
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
 
+    for i in &v {
+        println!("The value of i is: {i}");
+    }
+
+    for i in &mut v {
+        *i += 50;
+        println!("The value of i is: {i}");
+    }
+
+    let v_2 = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v_2[2];
+    println!("The third element is: {third}");
+
+    let third: Option<&i32> = v_2.get(2);
+    match third {
+        Some(third) => println!("The third element is: {third}"),
+        None => println!("There is no third element."),
+    }
+
+    // HashMaps
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Man_City"), 10);
+    scores.insert(String::from("Liverpool"), 9);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+    scores.entry(String::from("Chelsea")).or_insert(50);
+
+    let score = scores.get("Liverpool").copied().unwrap_or(0);
+    // println!("The score of Liverpool is: {score}");
+
+    let team_name = "Man_City";
+    let score = scores.get(team_name).copied().unwrap_or(0);
+    // println!("The score of {team_name} is: {score}");
+
+    for (key, value) in &scores {
+        println!("\t{key}: {value}");
+    }
+
+    let text = "what a wonderful world, wonderful";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{map:?}");
+}
 
 // Path: variables/src/main.rs
